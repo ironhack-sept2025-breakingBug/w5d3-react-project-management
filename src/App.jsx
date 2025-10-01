f20fd8941e03ee9f41193579ea3e8080ee807f6a
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-10-01 10:26:32 +0200

    feat: setup routing and first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import ProjectListPage from "./pages/ProjectListPage"
+
 function App() {
   return (
     <>
-      <h1>React Project Management</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectListPage />} />
+      </Routes>
     </>
   )
 }
