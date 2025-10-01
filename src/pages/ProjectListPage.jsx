3337b4c5804c8e7460ae1f0cf528854a3a69408a
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-10-01 11:00:22 +0200

    feat: implement list of projects

--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -1,8 +1,41 @@
+import { use, useEffect, useState } from 'react';
+import axios from 'axios'
+
+import Loader from '../components/Loader';
+
+const BASE_URL = 'https://project-management-api-2e731c3b8929.herokuapp.com'
+
+
 function ProjectListPage() {
+
+    const [projects, setProjects] = useState(null)
+
+    useEffect(() => {
+        axios.get(BASE_URL + "/projects")
+            .then(response => {
+                setProjects(response.data)
+            })
+            .catch(e => console.log("Error getting projects from the API...", e));
+    }, [])
+
+
+    if (projects === null) {
+        return <Loader />
+    }
 
     return (
         <div>
             <h1>Project List Page</h1>
+            <h2>Number of projects: {projects.length}</h2>
+
+            {projects.map((projectObj) => {
+                return (
+                    <div className="card" key={projectObj.id}>
+                        <h3>{projectObj.title}</h3>
+                        <p>{projectObj.description}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 }
